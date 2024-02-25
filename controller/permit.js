bce7543d50cc66461269c0236afed3d1ed5123e7
Nyi Nyi Zin <nyinyizin1818@gmail.com>
2024-02-26 01:27:58 +0630

    new commit

--- controller/permit.js
+++ controller/permit.js
@@ -1,32 +1,16 @@
 const DB = require("../models/permit");
+const Helper = require("../utils/helper");
 
 let add = async (req, res, next) => {
-  let data = new DB(req.body);
-  let result = await data.save();
-  res.send({ con: true, msg: "model Saved!", result });
-};
-let all = async (req, res, next) => {
-  let result = await DB.find();
-  res.send({ con: true, msg: "All models!", result });
-};
-let get = async (req, res, next) => {
-  let result = await DB.findById(req.params.id);
-  res.send({ con: true, msg: "Single model!", result });
-};
-let patch = async (req, res, next) => {
-  await DB.findByIdAndUpdate(req.params.id, req.body);
-  let result = await DB.findById(req.params.id);
-  res.send({ con: true, msg: "Model updated!", result });
-};
-let drop = async (req, res, next) => {
-  let result = await DB.findByIdAndDelete(req.params.id, { new: true });
-  res.send({ con: true, msg: "Model Deleted!", result });
+  let dbPermit = await DB.findOne({ name: req.body.name });
+  if (dbPermit) {
+    next(new Error("Permission name is already in use"));
+  } else {
+    let result = await new DB(req.body).save();
+    Helper.fMsg(res, "New Permit Added", result);
+  }
 };
 
 module.exports = {
   add,
-  all,
-  get,
-  patch,
-  drop,
 };
